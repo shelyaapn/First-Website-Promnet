bd6cd57ad3287ef487715d7ed57adaf78315ed2b
shelyaapn <putrishelya@gmail.com>
2022-10-11 23:58:45 +0700

    Tugas 4 jadi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const tari = ["Bali", "Lombok", "Labuan Bajo"];
+const sastri = ["Surabaya", "Bali", "Lombok"];
+const destinationTogether = getSolution(tari, sastri);
+
+function getSolution(array1, array2) {
+  return array1.filter(function (item) {
+    return array2.indexOf(item) !== -1;
+  });
+}
+function getResults(params) {
+  alert("Destination : " + destinationTogether);
+}
